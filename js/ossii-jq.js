ef08f7d154d90298514e8180d6c80dbead803d23
root <root@localhost.localdomain>
2012-09-11 11:14:57 +0800

    .

--- js/ossii-jq.js
+++ js/ossii-jq.js
@@ -11,5 +11,26 @@
         }).live('mouseleave',function(){
             $(this).removeClass('hover');
         });
+        
+        // fix sub nav on scroll 0910從bootstrap官網抓的
+        var $win = $(window)
+          , $nav = $('.subnav')
+          , navTop = $('.subnav').length && $('.subnav').offset().top - 40
+          , isFixed = 0
+    
+        processScroll()
+    
+        $win.on('scroll', processScroll)
+    
+        function processScroll() {
+          var i, scrollTop = $win.scrollTop()
+          if (scrollTop >= navTop && !isFixed) {
+            isFixed = 1
+            $nav.addClass('subnav-fixed')
+          } else if (scrollTop <= navTop && isFixed) {
+            isFixed = 0
+            $nav.removeClass('subnav-fixed')
+          }
+        }
     });        
 })(jQuery);
